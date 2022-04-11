ebc7b885f29f9ea82315da72730c5c3a8d54514b
sbongsu <92431853+sbongsu@users.noreply.github.com>
2022-04-11 20:26:00 +0900

    [Bronze IV] Title: 시험 성적, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in); 
+		
+		int A = scan.nextInt();
+		
+		if(90 <= A) {
+			System.out.println("A");
+		} else if(80 <= A) {
+			System.out.println("B");
+		} else if (70 <= A) {
+			System.out.println("C");
+		} else if(60 <= A){
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+
+}
